a00bf4cc4b7d7c0a3e5159b5c67f3e6980111620
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-14 17:12:55 -0300

    :sparkles:	feat: create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config()
+
+const Person = require('./models/Person');
+
+app.use(express.json());
+
+//rotas da API
+
+//rota inicial
+app.get('/', (_request, response) => response.status(200).json('Hello World!'));
+
+
+mongoose.connect(
+  process.env.URL_MONGO,
+)
+  .then(() =>
+    console.log(`Connected to MongoDB, port ${process.env.PORT}`),
+    app.listen(process.env.PORT)
+  )
+  .catch((error) => console.log(error));
